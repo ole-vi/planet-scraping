1299775ac6d91cda35c744b2ef53909baa2fb21e
Xavier Elon Hollingsworth <Xavierh93@hotmail.com>
2019-11-20 22:40:12 -0500

    Add files via upload

---
+++ planet.py
@@ -0,0 +1,65 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.support import ui
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+import time
+import os
+
+
+browser = webdriver.Firefox()
+browser.get('http://localhost:3100/eng/login?returnUrl=%2F')
+
+
+# Select username and password boxes
+username = browser.find_element_by_id('mat-input-0')
+password = browser.find_element_by_id('mat-input-1')
+
+# Get password and username from environment variable
+pword = os.getenv('LOGIN_PASSWORD')
+uname = os.getenv('LOGIN_USERNAME')
+
+# Send username and password from environment variables
+username.send_keys(uname)
+password.send_keys(pword)
+
+# Click login button
+browser.find_element_by_class_name('mat-raised-button').click()
+
+# Click add item button
+browser.find_element_by_id('cdk-drop-list-0').click()
+
+# Need this to allow angular to load 
+time.sleep(2)
+
+# Click resource "+" button
+browser.find_element_by_class_name('mat-mini-fab').click()
+
+time.sleep(2)
+
+# Upload file
+browse_button = browser.find_element_by_css_selector('div.inner-gaps:nth-child(17) > input:nth-child(2)')
+browser.execute_script("arguments[0].style.display = 'block';", browse_button)
+browse_button = browser.find_element_by_css_selector('div.inner-gaps:nth-child(17) > input:nth-child(2)')
+browse_button.send_keys('/Users/XavierElon/Documents/treehouses/planet-scraping/treehouses.zip')
+
+time.sleep(2)
+
+# Fill in "open which file" box
+which_file = browser.find_element_by_id('mat-input-9')
+which_file.send_keys("treehouses.github.io/index.md")
+
+# Fill in title field
+title = browser.find_element_by_id('mat-input-3')
+title.send_keys('treehouses.io')
+
+# Fill in subject drop - cannot get it to select an option in the dropdown
+subject = browser.find_element_by_css_selector('#mat-select-2').click
+for option in subject.find_elements_by_tag_name('aria-owns'):
+	if option.text == "Agriculture":
+		option.click()
+		break
+
+# Click submit button
+# browser.find_element_by_class_name('mat-primary').click()
+
---
+++ planet.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# pip3 install -U selenium
+
+git clone https://github.com/treehouses/treehouses.github.io.git
+zip -r treehouses.zip treehouses.github.io
+
+# python3 pythonscrape.py
+
+#curl -T treehouses.github.io http://localhost:5984/_utils/#database/resources/_all_docs
+
+#curl -X PUT -T /Users/XavierElon/Library/Mobile\ Documents/com~apple~CloudDocs/hacking/python/treehouses.github.io http://localhost:5984/resources/treehouses -u xavierelon:toor
